4ebcc5be6ede1529f04007cfb487278a9648a8d7
Benjamin Hartmann <42138517+benjaminwolkchen@users.noreply.github.com>
2021-03-23 14:22:47 +0100

    Rewrite if cases for missing user input

--- api/auth.php
+++ api/auth.php
@@ -10,7 +10,7 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $remember = $_POST['remember_me'];
 
-if(!filter_var($email, FILTER_VALIDATE_EMAIL) || strlen($passwort) == 0 || strlen($invite_code) == 0) {
+if(strlen($invite_code) == 0 || !filter_var($email_a, FILTER_VALIDATE_EMAIL) || strlen($password) == 0) {
  // Todo: Handle missing user input.
 }else{
     $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
