42c0dc53831e2c47b87ac6bb697be16784d892cb
Benjamin H <hbenjamin@tutanota.de>
2021-10-03 13:15:33 +0200

    Serverstruktur geschaffen

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const app = express();
 app.get('/', (req, res) => {
   res.redirect("https://codeberg.org/awesomebible/my-backend");
 });
+
+app.get('/timeline', (req, res) => {
+    // Fetch timeline
+  });
+app.post('/auth', (req, res) => {
+    // Do Auth
+});
+app.post('/reset-password', (req, res) => {
+    // Do Auth
+});
+app.post('/create-file', (req, res) => {
+    // Create File
+});
+app.post('/create-post', (req, res) => {
+    // Create Post
+});
+
+app.listen(3000, () => console.log('my.awesomeBible Server is listening on port 3000.'));
